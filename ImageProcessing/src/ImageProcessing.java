6dcfeefddf54dab04c20d28fcff4dedc7617f090
Dennis van der Bij <dennis.vanderbij@epfl.ch>
2015-05-06 09:22:13 +0200

    Sort lines.

--- ImageProcessing/src/ImageProcessing.java
+++ ImageProcessing/src/ImageProcessing.java
@@ -2,6 +2,9 @@ import processing.core.PApplet;
 import processing.core.PImage;
 import processing.video.Capture;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class ImageProcessing extends PApplet {
     Capture cam;
     private PImage img;
@@ -9,32 +12,35 @@ public class ImageProcessing extends PApplet {
     private HScrollbar colorBar;
 
     public void setup() {
-        size(640, 480);
-        String[] cameras = Capture.list();
-        if (cameras.length == 0) {
-            println("There are no cameras available for capture.");
-            exit(); } else {
-            println("Available cameras:");
+//        size(640, 480);
+//        String[] cameras = Capture.list();
+//        if (cameras.length == 0) {
+//            println("There are no cameras available for capture.");
+//            exit(); } else {
+//            println("Available cameras:");
+//
+//            for (int i = 0; i < cameras.length; i++) {
+//                println(cameras[i]);
+//            }
+//
+//            cam = new Capture(this, cameras[0]);
+//            cam.start();
+//        }
 
-            for (int i = 0; i < cameras.length; i++) {
-                println(cameras[i]);
-            }
-
-            cam = new Capture(this, cameras[0]);
-            cam.start();
-        }
+        size(800, 600);
+        img = loadImage("board1.jpg");
 
         brightnessBar = new HScrollbar(this, 0, 580, 800, 20);
         colorBar = new HScrollbar(this, 0, 560, 800, 20);
     }
 
     public void draw() {
-        if (cam.available() == true) {
-            cam.read();
-        }
-
-        img = cam.get();
-        image(img, 0, 0);
+//        if (cam.available() == true) {
+//            cam.read();
+//        }
+//
+//        img = cam.get();
+//        image(img, 0, 0);
 
         background(color(0, 0, 0));
 
@@ -51,9 +57,11 @@ public class ImageProcessing extends PApplet {
             }
         }
 
-        result = gaussianBlur(result);
+        for (int i = 0; i < 3; i++) {
+            result = gaussianBlur(result);
+        }
         result = sobel(result);
-        image(img, 0, 0);
+        image(result, 0, 0);
         result = hough(result);
 
 //        image(result, 0, 0);
@@ -69,20 +77,20 @@ public class ImageProcessing extends PApplet {
 
     private PImage gaussianBlur(PImage img) {
         float[][] kernel = {{9, 12, 9},
-                            {12, 15, 12},
-                            {9, 12, 9}};
+                {12, 15, 12},
+                {9, 12, 9}};
 
         return convolute(img, kernel);
     }
 
     private PImage sobel(PImage img) {
         float[][] hKernel = {{0, 1, 0},
-                             {0, 0, 0},
-                             {0, -1, 0}};
+                {0, 0, 0},
+                {0, -1, 0}};
 
         float[][] vKernel = {{0, 0, 0},
-                             {1, 0, -1},
-                             {0, 0, 0}};
+                {1, 0, -1},
+                {0, 0, 0}};
 
         int N = 3;
         float max = 0;
@@ -159,8 +167,6 @@ public class ImageProcessing extends PApplet {
             }
         }
 
-        plotLines(img, discretizationStepsPhi, discretizationStepsR, rDim, accumulator);
-
 //        PImage houghImg = createImage(rDim + 2, phiDim + 2, ALPHA);
 //        for (int i = 0; i < accumulator.length; i++) {
 //            houghImg.pixels[i] = color(min(255, accumulator[i]));
@@ -169,51 +175,63 @@ public class ImageProcessing extends PApplet {
 //        houghImg.updatePixels();
 //        houghImg.resize(640, 480);
 
+        ArrayList<Integer> bestCandidates = new ArrayList<Integer>();
+
+        for (int i = 0; i < ((phiDim + 2) * (rDim + 2)); i++) {
+            bestCandidates.add(new Integer(i));
+        }
+
+        Collections.sort(bestCandidates, new HoughComparator(accumulator));
+
+        plotLines(bestCandidates, img.width);
+
         return null;
     }
 
-    private void plotLines(PImage img, float discretizationStepsPhi, float discretizationStepsR, float rDim, int[] accumulator) {
-        for (int i = 0; i < accumulator.length; i++) {
-            if (accumulator[i] > 300) {
-                // first, compute back the (r, phi) polar coordinates:
-                int accPhi = (int) (i / (rDim + 2)) - 1;
-                int accR = (int) (i - (accPhi + 1) * (rDim + 2) - 1);
-                float r = (accR - (rDim - 1) * 0.5f) * discretizationStepsR;
-                float phi = accPhi * discretizationStepsPhi;
+    private void plotLines(ArrayList<Integer> bestCandidates, int width) {
+        float discretizationStepsPhi = 0.06f;
+        float discretizationStepsR = 2.5f;
+        int rDim = (int) (((img.width + img.height) * 2 + 1) / discretizationStepsR);
 
-                // Cartesian equation of a line: y = ax + b
-                // in polar, y = (-cos(phi)/sin(phi))x + (r/sin(phi))
-                // => y = 0 : x = r / cos(phi)
-                // => x = 0 : y = r / sin(phi)
-                // compute the intersection of this line with the 4 borders of // the image
-                int x0 = 0;
-                int y0 = (int) (r / sin(phi));
-                int x1 = (int) (r / cos(phi));
-                int y1 = 0;
-                int x2 = img.width;
-                int y2 = (int) (-cos(phi) / sin(phi) * x2 + r / sin(phi));
-                int y3 = img.width;
-                int x3 = (int) (-(y3 - r / sin(phi)) * (sin(phi) / cos(phi)));
+        for (Integer i : bestCandidates.subList(0, 30)) {
+            // first, compute back the (r, phi) polar coordinates:
+            int accPhi = (int) (i / (rDim + 2)) - 1;
+            int accR = (int) (i - (accPhi + 1) * (rDim + 2) - 1);
+            float r = (accR - (rDim - 1) * 0.5f) * discretizationStepsR;
+            float phi = accPhi * discretizationStepsPhi;
 
-                // Finally, plot the lines
-                stroke(204,102,0); if (y0 > 0) {
-                    if (x1 > 0) {
-                        line(x0, y0, x1, y1);
-                    } else if (y2 > 0) {
-                        line(x0, y0, x2, y2);
+            // Cartesian equation of a line: y = ax + b
+            // in polar, y = (-cos(phi)/sin(phi))x + (r/sin(phi))
+            // => y = 0 : x = r / cos(phi)
+            // => x = 0 : y = r / sin(phi)
+            // compute the intersection of this line with the 4 borders of // the image
+            int x0 = 0;
+            int y0 = (int) (r / sin(phi));
+            int x1 = (int) (r / cos(phi));
+            int y1 = 0;
+            int x2 = width;
+            int y2 = (int) (-cos(phi) / sin(phi) * x2 + r / sin(phi));
+            int y3 = width;
+            int x3 = (int) (-(y3 - r / sin(phi)) * (sin(phi) / cos(phi)));
+
+            // Finally, plot the lines
+            stroke(204,102,0); if (y0 > 0) {
+                if (x1 > 0) {
+                    line(x0, y0, x1, y1);
+                } else if (y2 > 0) {
+                    line(x0, y0, x2, y2);
+                } else {
+                    line(x0, y0, x3, y3);
+                }
+            } else {
+                if (x1 > 0) {
+                    if (y2 > 0) {
+                        line(x1, y1, x2, y2);
                     } else {
-                        line(x0, y0, x3, y3);
+                        line(x1, y1, x3, y3);
                     }
                 } else {
-                    if (x1 > 0) {
-                        if (y2 > 0) {
-                            line(x1, y1, x2, y2);
-                        } else {
-                            line(x1, y1, x3, y3);
-                        }
-                    } else {
-                        line(x2, y2, x3, y3);
-                    }
+                    line(x2, y2, x3, y3);
                 }
             }
         }
